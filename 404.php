440bf674e7175c0b9a67a66b9a42caa68813946e
Andrija Nikolic <andrijanikolic1984@gmail.com>
2019-06-21 15:49:32 +0200

    Update 404 page

--- 404.php
+++ 404.php
@@ -1,24 +1,14 @@
 <?php
 
-get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-
-			<header class="page-header">
-				<h1 class="page-title"><?php _e( 'Not Found', 'twentythirteen' ); ?></h1>
-			</header>
-
-			<div class="page-wrapper">
-				<div class="page-content">
-					<h2><?php _e( 'This is somewhat embarrassing, isn’t it?', 'twentythirteen' ); ?></h2>
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen' ); ?></p>
-
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</div><!-- .page-wrapper -->
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
-
-<?php get_footer(); ?>
+ get_header(); ?> 
+<body <?php body_class(); ?>>
+<?php get_template_part( 'nav' ); // Navigation bar (nav.php) ?>              
+<div class="homepage">
+            <div class="container">
+                    <div style="height:32px;"></div>
+					<div data-aos="fade-up" data-aos-once="false" data-aos-easing="ease-in-out" data-aos-duration="1000">
+                    <h1>UH, NOTHING TO DISPLAY. TRY ANOTHER PAGE</h1>
+					<div>
+                    <div style="height:32px;"></div>                                    
+            </div>			
+<?php	get_footer(); ?> 
