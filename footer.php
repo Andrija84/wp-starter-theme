32ce486dd9b4af5b29bc1fca21966b6803a69fb1
Andrija Nikolic <andrijanikolic1984@gmail.com>
2019-06-28 18:47:21 +0200

    Remove Admin Widgets

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
                     
                     
                     <div class="footer-left" style="">
-						<strong style="color:#000;">OOMPA DESIGN</strong> © <?php echo wpb_copyright(); ?> Alle Rechte vorbehalten.
+						<strong style="color:#000;">OOMPA DESIGN</strong>  <?php echo wpb_copyright(); ?> Alle Rechte vorbehalten.
 					</div>
                     <div class="footer-right" style="">
 						
--- functions.php
+++ functions.php
@@ -1,10 +1,26 @@
 <?php
 
+require_once( __DIR__ . '/functions/admin.php');
+
 //REMOVES ADMIN TOP FROM FRONT
 show_admin_bar( false );
 
 //REMOVES WELCOME DASH PANEL
 remove_action('welcome_panel', 'wp_welcome_panel');
+
+/* REMOVE ADMIN DASHBOARD WIDGETS */
+function remove_dashboard_meta() {
+        remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+        remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+        remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+        remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
+        remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+        remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+        remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+        remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+        remove_meta_box( 'dashboard_activity', 'dashboard', 'normal');//since 3.8
+}
+add_action( 'admin_init', 'remove_dashboard_meta' );
 
 //DISABLE XML-RPC	
 add_filter('xmlrpc_enabled', '__return_false');
@@ -19,7 +35,7 @@ add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
 add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
 
 
-/* REMOVE TAHANK YOU NOTIFICATION FROM DASHBOARD */
+/* REMOVE THANK YOU NOTIFICATION FROM DASHBOARD */
  function remove_footer_admin (){
     echo '<span id="footer-thankyou">Developed by <a href="https://oompa.de" target="_blank">Andrija Nikolic</a></span>';
 }
@@ -608,9 +624,6 @@ add_action('init','wpb_admin_account');
 
 
 /* REMOVES COMMENTS */
- 
-// Removes from admin menu
-
 function my_remove_admin_menus() {
     remove_menu_page( 'edit-comments.php' );
 }
